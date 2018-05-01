585a8c0d09e4f1fe6ca36964fc9dafb1659870a7
charinda <charinda04@gmail.com>
2018-05-01 21:18:58 +0530

    start project manager

---
+++ resources/views/companies/show.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="col-md-9 col-lg-9 pull-left col-sm-3">
+
+
+  <div class="container">
+
+      <!-- The justified navigation menu is meant for single line per list item.
+       Multiple lines will require custom code not provided by Bootstrap. -->
+
+
+       <!-- Jumbotron -->
+       <div class="jumbotron">
+        <h1>{{ $company->name }}</h1>
+        <p class="lead">{{ $company->description }}</p>
+        <!-- <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p> -->
+      </div>
+
+      <!-- Example row of columns -->
+      <div class="row" style="background: white; margin: 10px">
+        @foreach($company->projects as $project)
+        <div class="col-lg-4">
+          <h2>{{ $project->name }}</h2>
+          <p class="text-danger">{{ $project->description }} </p>
+          <p><a class="btn btn-primary" href="/projects/{{ $project->id }}" role="button">View Project »</a></p>
+        </div>
+        @endforeach
+      </div>
+
+      <!-- Site footer -->
+      <footer class="footer">
+        <p>© 2016 Company, Inc.</p>
+      </footer>
+
+    </div>
+  </div>
+
+  <div class="col-sm-3 col=md-3 col-lg-3 pull-right">
+          <!-- <div class="sidebar-module sidebar-module-inset">
+            <h4>About</h4>
+            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </div> -->
+          <div class="sidebar-module">
+            <h4>Actions</h4>
+            <ol class="list-unstyled">
+              <li><a href="#">Edit</a></li>
+              <li><a href="#">Delete</a></li>
+              <li><a href="#">Add New Member</a></li>
+            </ol>
+          </div>
+
+          <div class="sidebar-module">
+            <h4>Members</h4>
+            <ol class="list-unstyled">
+              <li><a href="#">March 2014</a></li>
+
+            </div>
+
+          </div>
+@endsection
